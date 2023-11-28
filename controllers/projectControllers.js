264309707f6429dab5f183ac2fe589745c7c3421
Ankit Meshram <ankitdm69@gmail.com>
2023-11-28 15:19:47 +0530

    temp

--- controllers/projectControllers.js
+++ controllers/projectControllers.js
@@ -58,3 +58,54 @@ exports.allProject = async (req, res) => {
     });
   }
 };
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const {
+      _id,
+      name,
+      start_date,
+      end_date,
+      owner,
+      description,
+      project_access,
+      created_date,
+      updated_date,
+    } = req.body;
+
+    const body = {
+      _id,
+      name,
+      start_date,
+      end_date,
+      owner,
+      description,
+      project_access,
+      created_date,
+      updated_date,
+    };
+
+    console.log("body", body);
+
+    if (!name || !owner) {
+      return res.status(400).json({
+        success: false,
+        message: "Name and Owner fields are required",
+      });
+    }
+    // console.log("Product Id", _id);
+    const project = await Project.findByIdAndUpdate(_id, body);
+    // console.log(product, "product");
+    return res.status(200).json({
+      success: true,
+      message: "Project Updated Successfully",
+      project: body,
+    });
+  } catch (error) {
+    // console.log("error", error);
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/Project.js
+++ routes/Project.js
@@ -4,10 +4,13 @@ const router = express.Router();
 const {
   createProject,
   allProject,
+  updateProduct,
 } = require("../controllers/projectControllers");
 
 router.post("/createproject", createProject);
 
 router.post("/allprojects", allProject);
+
+router.post("/updateproject", updateProduct);
 
 module.exports = router;
