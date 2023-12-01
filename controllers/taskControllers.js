d5736e9a7c9eb0b4ce8dc6ce7c36fa7ebdd174e7
Ankit Meshram <ankitdm69@gmail.com>
2023-12-01 19:57:35 +0530

    task functionality created

---
+++ controllers/taskControllers.js
@@ -0,0 +1,179 @@
+const Task = require("../models/Task");
+
+exports.createTask = async (req, res) => {
+  try {
+    const {
+      pid,
+      title,
+      description,
+      assignTo,
+      assignBy,
+      dueDate,
+      priority,
+      isCompleted,
+      comments,
+      created_at,
+      updated_at,
+    } = req.body;
+    if (!pid || !title) {
+      return res.status(400).json({
+        success: false,
+        message: "Project Id and Task Title fields are required",
+      });
+    }
+    const TaskDetails = await Task.create({
+      pid,
+      title,
+      description,
+      assignTo,
+      assignBy,
+      dueDate,
+      priority,
+      isCompleted,
+      comments,
+      created_at: created_at,
+      updated_at: updated_at,
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Tasks Created Successfully",
+      ProjectDetails,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.allTask = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json({
+      success: true,
+      tasks: tasks,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error?.message,
+    });
+  }
+};
+
+exports.updateProject = async (req, res) => {
+  try {
+    const {
+      _id,
+      pid,
+      title,
+      description,
+      assignTo,
+      assignBy,
+      dueDate,
+      priority,
+      isCompleted,
+      comments,
+      created_at,
+      updated_at,
+    } = req.body;
+
+    const body = {
+      _id: _id,
+      pid: pid,
+      title: title,
+      description: description,
+      assignTo: assignTo,
+      assignBy: assignBy,
+      dueDate: dueDate,
+      priority: priority,
+      isCompleted: isCompleted,
+      comments: comments,
+      created_at: created_at,
+      updated_at: updated_at,
+    };
+
+    if (!_id || !pid || !title) {
+      return res.status(400).json({
+        success: false,
+        message: "Id and pid and title fields are required",
+      });
+    }
+    const Task = await Task.findByIdAndUpdate(_id, body);
+    return res.status(200).json({
+      success: true,
+      message: "Task Updated Successfully",
+      task: body,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.viewProjectByAccess = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const projects = await Project.find({
+      $or: [{ owner: email }, { project_team: { $in: [email] } }],
+    });
+
+    if (projects.length == 0) {
+      return res.status(200).json({
+        success: true,
+        message: "No Projects Found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Projects found successfully",
+      projects,
+    });
+  } catch (err) {
+    return res.status(504).json({
+      success: false,
+      message: `Something went wrong ${err}`,
+    });
+  }
+};
+
+exports.deleteProject = async (req, res) => {
+  try {
+    const { _id, email } = req.body;
+    const project = await Project.findById({ _id: _id });
+
+    if (!project) {
+      return res.status(404).json({
+        success: false,
+        message: "project not found",
+      });
+    }
+
+    console.log("project", project);
+
+    if (project?.owner == email) {
+      await Project.findByIdAndDelete({ _id: _id });
+      res.status(200).json({
+        success: true,
+        message: "project deleted successfully",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "You don't have access to delete",
+      });
+    }
+  } catch (error) {
+    // console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "project Cannot be deleted successfully",
+      error,
+    });
+  }
+};
---
+++ models/Task.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  id: {
+    type: String,
+  },
+  pid: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  assignTo: {
+    type: String,
+  },
+  assignBy: {
+    type: String,
+  },
+  dueDate: {
+    type: date,
+  },
+  priority: {
+    type: String,
+  },
+  isCompleted: {
+    type: String,
+  },
+  comments: [
+    {
+      by: {
+        type: String,
+      },
+      type: {
+        type: String,
+      },
+    },
+  ],
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
